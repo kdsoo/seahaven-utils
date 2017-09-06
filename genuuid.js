d9669aec2e6f40579cf7897c4307096b7871d5c0
Dongsoo Kim <dongsoo.kim@gmail.com>
2017-09-06 16:23:04 +0900

    service uuid generator revised
    Signed-off-by: Dongsoo Kim <dongsoo.kim@gmail.com>

--- genuuid.js
+++ genuuid.js
@@ -5,23 +5,25 @@ var servicedomain = "https://www.seahaven.xyz/";
 var path = require('path')
 
 if (process.argv.length < 3) {
-	console.error('Usage: %s "service name"', path.basename(process.argv[1]))
+	console.error('Usage: %s "service name" <option>', path.basename(process.argv[1]))
 		process.exit(1)
 }
 var name = process.argv[2];
-
+var verbose = process.argv[3] == "-v" ? true:false;
 
 UUID.v3({
 	namespace: UUID.namespace.url,
 	name: servicedomain
 }, function (err, result) {
-	console.log("Service namespace for name " + name + ": " + result);
-	console.log(UUID.check(result));
+	if (verbose {
+		console.log("Service namespace for name " + name + ": " + result);
+		console.log(UUID.check(result));
+	}
 
 	var data = { namespace: result, name: name };
 	UUID.v5(data, function (err, result) {
 		console.log("Generated a name-based UUID using SHA1:", result);
-		console.log(UUID.check(result));
+		if (verbose) console.log(UUID.check(result));
 	});
 });
 
